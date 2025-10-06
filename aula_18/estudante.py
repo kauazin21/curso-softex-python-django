b89754c8c66913f4fa26b670a869371ef463e021
kauazin21 <kauamendess1602@gmail.com>
2025-10-06 10:44:01 -0300

    função mostrar_informacoes( ) foi adicionada

--- aula_18/estudante.py
+++ aula_18/estudante.py
@@ -1,21 +1,25 @@
 from pessoa import Pessoa
 
-materias:dict[str, list[float]] = {}
-
 class Estudante(Pessoa):
     def __init__(self, nome: str, idade: int, matricula: str):
         super().__init__(nome, idade)
         self.matricula = matricula
+        self.materias:dict[str, list[float]] = {}
 
     def add_nota_materia(self, materia: str, nota:float):
         self.materia = materia
         self.nota = nota
-        aula = materias.get(self.materia)
+        aula = self.materias.get(self.materia)
         print(f"aula atual: {aula}")
         if aula:
             aula.append(self.nota)
         else:
-            materias[self.materia] = [self.nota]
+            self.materias[self.materia] = [self.nota]
         
-        print(f"dicionario de materias: {materias}")
+        print(f"dicionario de materias: {self.materias}")
 
+    def mostrar_informacoes(self):
+        print(f"Nome: {self.nome} | Idade: {self.idade} | Matrícula: {self.matricula}")
+        for materia, notas in self.materias.items():
+            media = sum(notas) / len(notas)
+            print(f"  Matéria: {materia} | Notas: {notas} | Média: {media:.2f}")
