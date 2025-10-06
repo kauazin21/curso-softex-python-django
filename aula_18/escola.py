fa7f62c20dcda1c02115badd42715b59960afd97
kauazin21 <kauamendess1602@gmail.com>
2025-10-06 10:45:59 -0300

    funções add_estudante( ) e mostrar_relatorio( ), foram corrigidas

--- aula_18/escola.py
+++ aula_18/escola.py
@@ -1,6 +1,15 @@
 from estudante import Estudante
 
 class Escola:
-    def __init__(self, estudantes: list[Estudante]):
-        self.estudantes = estudantes
-        
+    def __init__(self, nome: str):
+        self.nome = nome
+        self.estudantes: list[Estudante] = []
+    
+    def add_estudante(self, estudante: Estudante):
+        self.estudantes.append(estudante)
+
+    def mostrar_relatorio(self):
+        print(f"=== Relatório da Escola {self.nome} ===")
+        for estudante in self.estudantes:
+            estudante.mostrar_informacoes()
+            print("-" * 40)
