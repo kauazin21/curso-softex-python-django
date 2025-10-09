11cba0e272bece70511fb636b6eed275beede6fa
kauazin21 <kauamendess1602@gmail.com>
2025-10-09 19:17:50 -0300

    verificador de par ou ímpar

---
+++ modulo_01/aula_02/exercicio_06.py
@@ -0,0 +1,8 @@
+#Verificador de Par ou ímpar
+num = int(input("Digite um número inteiro positivo: "))
+
+if num % 2 == 0:
+    print(f"O número {num} é par!")
+
+else:
+    print(f"O número {num} é impar!")
