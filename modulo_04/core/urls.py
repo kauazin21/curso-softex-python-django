d6889aa6df63c266e59f51f15a48665ce3e12c69
kauazin <167949109+kauazin21@users.noreply.github.com>
2025-11-10 15:26:38 -0300

    feat: atualizações

--- modulo_04/core/urls.py
+++ modulo_04/core/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views # O '.' importa as 'views' do app atual
 urlpatterns = [
 # Quando a URL for a raiz (''), chame a função 'home' de 'views.py'
-path('home/', views.home, name='home'),
-path('', views.funcao, name='função')
+path('', views.home, name='home'),
 ]
--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Uma 'view' é uma função que recebe um 'request' e retorna uma 'response'
-def home(request):
-# Vamos retornar a resposta HTTP mais simples: um texto HTML
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
 
-def funcao(request):
-    return HttpResponse("<h2>Testando...</h2>")
+def home(request):
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+    }
+    
+    return render(request, 'home.html', context)
